f5741c2b0d19a1591615d08ecc35c344668b8fd5
hoyho <luohaihao@gmail.com>
2024-01-23 17:33:40 +0800

    fix picker not display value in macos
    Signed-off-by: hoyho <luohaihao@gmail.com>

---
+++ Views/GeneralPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace iTimeSlot.Views;
+
+public partial class GeneralPage : ContentPage
+{
+	List<string> _remind_befores = new List<string> { "1 minute before", "2 minute before", "5 minute before", "10 minute before" }; 
+	int count = 0;
+
+	public GeneralPage()
+	{
+		InitializeComponent();
+		
+		// set the default value for the picker because the bug in macOS that not binding the value
+		//see also https://github.com/dotnet/maui/issues/10208
+		picker.SelectedIndex = 1;
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+	}
+}
+
