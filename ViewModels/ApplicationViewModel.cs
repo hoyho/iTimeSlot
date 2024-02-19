ee370cdd9da970c9e5d9398b235c3e7afc1a5402
hoyho <luohaihao@gmail.com>
2024-02-20 00:05:46 +0800

    feat: add tray icon demo
    Signed-off-by: hoyho <luohaihao@gmail.com>

---
+++ ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,44 @@
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using CommunityToolkit.Mvvm.Input;
+using iTimeSlot_avalonia.Views;
+
+namespace iTimeSlot_avalonia.ViewModels;
+
+public partial class ApplicationViewModel:ViewModelBase
+{
+    
+        
+    [RelayCommand]
+    private void ShowWindow()
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            var mainWin = desktop.MainWindow;
+            if (mainWin != null)
+            {
+                mainWin.WindowState = WindowState.Normal;
+                mainWin.Show();
+                mainWin.BringIntoView();
+                mainWin.Focus();    
+            }
+            else
+            {
+                Console.WriteLine("main windows was not inited");
+            }
+
+        }
+    }
+
+
+    [RelayCommand]
+    private static void Exit()
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime application)
+        {
+            application.Shutdown();
+        }
+    }
+}
