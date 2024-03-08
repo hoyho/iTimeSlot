c580dfb7ce2bdccc0bbdff98e6eced0671583ea2
hoyho <luohaihao@gmail.com>
2024-03-08 23:20:49 +0800

    refactor: split workspace tab to individual control
    Signed-off-by: hoyho <luohaihao@gmail.com>

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,9 @@
-﻿
-
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-
-namespace iTimeSlot.ViewModels;
+﻿namespace iTimeSlot.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
-
-    public List<TimeSpan> ExistingTimeSpans => Shared.Global.ExistTimeSpans;
 
 }
--- ViewModels/WorkspaceTabViewModel.cs
+++ ViewModels/WorkspaceTabViewModel.cs
@@ -2,10 +2,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace iTimeSlot.ViewModels;
 
-public class SettingTabViewModel : ViewModelBase
+public class WorkspaceTabViewModel : ViewModelBase
 {
+
     public List<TimeSpan> ExistingTimeSpans => Shared.Global.ExistTimeSpans;
+
 }
